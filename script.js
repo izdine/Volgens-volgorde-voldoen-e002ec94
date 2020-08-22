471b03447390d70682875179c2790229bbbb57c9
Iz-Dine Amadou <2088346@talnet.nl>
2020-08-22 16:04:56 +0200

    Uitwerking van de exercise

--- script.js
+++ script.js
@@ -2,33 +2,37 @@ const antwoorden = {
     Frankrijk : "Parijs",
     spin : "8",
     meer : "IJsselmeer",
-    automerk : ["Volkswagen", "Audi", "Opel", "Porsche", "BMW", "Mercedes", "Mercedes-Benz"],
-   waddeneiland :["Texel", "Vlieland", "Terschelling", "Ameland", "Schiermonnikoog"]
+    automerk : ["Opel"],
+   waddeneiland :[ "Ameland"]
 };
 
 function check(){
 let allsgoed = true;
+let points = -20;
 
-Object.keys(antwoorden).forEach(function (item) {
+Object.keys(antwoorden).forEach(function(item) {
 let element = document.getElementById(item)
 if ( antwoorden[item].includes(element.value)) {
  let input = document.getElementsByTagName("input")
 for (i = 0; i < input.length; i++) {
     input[i].style.backgroundColor = "green";
+     points++;
   }
-
 }else {
   let input = document.getElementsByTagName("input")
   for (i = 0; i < input.length; i++) {
      input[i].style.backgroundColor = "red";
+
    }
   allsgoed = false
 document.getElementById("juist").innerText ='😥je hebt een paar fouten😥'
 }
 });
-
 if (allsgoed) {
-  document.getElementById("juist").innerText = "🎊🎉Alle antwoorden zijn correct🎊🎉✌️"
+  document.getElementById("juist").innerText = "🎊🎉Alle antwoorden zijn correct🎊🎉"
+  document.getElementById("cijfer").innerHTML =  points;
   }
-
+}
+function resultaat() {
+  document.getElementById("resultaat").innerText = "Je hebt er " + points() + " goed";
 }
