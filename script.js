b3cd725ee4e79f2fac58b74878234d43c3056d9e
Izdine Amadou <you@example.com>
2020-09-05 13:07:01 +0200

    Uitwerking van de exercise

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 const antwoorden = {
-Frankrijk : ['Parijs', 'parijs'],
-spin : ['8', 'acht'],
-meer : ['IJsselmeer', 'ijsselmeer'],
-automerk : ['Volkswagen', 'Audi', 'Opel', 'Porsche', 'BMW', 'Mercedes', 'Mercedes-Benz'],
-waddeneiland :['Texel', 'Vlieland', 'Terschelling', 'Ameland', 'Schiermonnikoog']
+ Frankrijk : ['Parijs', 'parijs'],
+ spin : ['8', 'acht'],
+ meer : ['IJsselmeer', 'ijsselmeer'],
+ automerk : ['Volkswagen', 'Audi', 'Opel', 'Porsche', 'BMW', 'Mercedes', 'Mercedes-Benz'],
+ waddeneiland :['Texel', 'Vlieland', 'Terschelling', 'Ameland', 'Schiermonnikoog']
 };
 
 function check(){
@@ -14,26 +14,26 @@ let pointsf = 0;
 Object.keys(antwoorden).forEach(function(item) {
 let element = document.getElementById(item)
 if ( antwoorden[item].includes(element.value)) {
-let input = document.getElementsByTagName("input")
+let input = document.getElementsByTagName('input')
 for (i = 0; i < input.length; i++) {
     pointsg++;
-    input[i].style.backgroundColor = "green";
+    input[i].style.backgroundColor = 'green';
 
   }
 }else {
-  let input = document.getElementsByTagName("input")
+  let input = document.getElementsByTagName('input')
   for (i = 0; i < input.length; i++) {
-     input[i].style.backgroundColor = "red";
+     input[i].style.backgroundColor = 'red';
          pointsf++;
   }
   allsgoed = false
-document.getElementById("juist").innerText ='😥je hebt een paar fouten😥'
-document.getElementById("cijfer").innerHTML =  pointsf +  'min punten' ;
+document.getElementById('juist').innerText ='😥je hebt een paar fouten😥'
+document.getElementById('cijfer').innerHTML =  pointsf +  'min punten' ;
  }
 });
 
 if (allsgoed) {
-  document.getElementById("juist").innerText = '🎊🎉Alle antwoorden zijn correct🎊🎉'
-  document.getElementById("cijfer').innerHTML =  pointsg + ' puls punten ' ;
+  document.getElementById('juist').innerText = '🎊🎉Alle antwoorden zijn correct🎊🎉'
+  document.getElementById('cijfer').innerHTML =  pointsg + ' puls punten ' ;
   }
 }
