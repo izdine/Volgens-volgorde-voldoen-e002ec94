ac35e539f6cbd4c6c901ded7e299a907052f2d17
Izdine Amadou <you@example.com>
2020-09-05 13:05:01 +0200

    Uitwerking van de exercise

--- script.js
+++ script.js
@@ -1,38 +1,39 @@
 const antwoorden = {
-    Frankrijk : "Parijs",
-    spin : "8",
-    meer : "IJsselmeer",
-    automerk : ["Opel"],
-   waddeneiland :[ "Ameland"]
+Frankrijk : ['Parijs', 'parijs'],
+spin : ['8', 'acht'],
+meer : ['IJsselmeer', 'ijsselmeer'],
+automerk : ['Volkswagen', 'Audi', 'Opel', 'Porsche', 'BMW', 'Mercedes', 'Mercedes-Benz'],
+waddeneiland :['Texel', 'Vlieland', 'Terschelling', 'Ameland', 'Schiermonnikoog']
 };
 
 function check(){
 let allsgoed = true;
-let points = -20;
+let pointsg = 0;
+let pointsf = 0;
 
 Object.keys(antwoorden).forEach(function(item) {
 let element = document.getElementById(item)
 if ( antwoorden[item].includes(element.value)) {
- let input = document.getElementsByTagName("input")
+let input = document.getElementsByTagName("input")
 for (i = 0; i < input.length; i++) {
+    pointsg++;
     input[i].style.backgroundColor = "green";
-     points++;
+
   }
 }else {
   let input = document.getElementsByTagName("input")
   for (i = 0; i < input.length; i++) {
      input[i].style.backgroundColor = "red";
-
-   }
+         pointsf++;
+  }
   allsgoed = false
 document.getElementById("juist").innerText ='😥je hebt een paar fouten😥'
-}
+document.getElementById("cijfer").innerHTML =  pointsf +  'min punten' ;
+ }
 });
+
 if (allsgoed) {
-  document.getElementById("juist").innerText = "🎊🎉Alle antwoorden zijn correct🎊🎉"
-  document.getElementById("cijfer").innerHTML =  points;
+  document.getElementById("juist").innerText = '🎊🎉Alle antwoorden zijn correct🎊🎉'
+  document.getElementById("cijfer').innerHTML =  pointsg + ' puls punten ' ;
   }
-}
-function resultaat() {
-  document.getElementById("resultaat").innerText = "Je hebt er " + points() + " goed";
 }
